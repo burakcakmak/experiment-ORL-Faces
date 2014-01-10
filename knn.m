24e767a01fb6d974ac2bd543fb696b0b28ddbddc
Hasan Imam <hasan.imam.w@gmail.com>
2014-01-10 16:01:38 -0500

    Added tests for ClassificationKNN

--- knn.m
+++ knn.m
@@ -13,13 +13,16 @@ for i = 1:subjects
     end
 end
 
+
 %% Define data sets
 % Define training set
-train_X = X(:, [1:8], :); % drop the 10th sample from training
-train_Y = Y(:, [1:8], :);
+tmp = randperm(10,2);
+train_X = X(:, ~ismember([1:10], tmp), :); % drop the 10th sample from training
+train_Y = Y(:, ~ismember([1:10], tmp), :);
 % Define test set
-test_X = X(:, [9, 10], :); % use the 10th sample for testing
-test_Y = Y(:, [9, 10], :);
+test_X = X(:, tmp, :); % use the 10th sample for testing
+test_Y = Y(:, tmp, :);
+
 
 %% Reshape data in appropriate sizes to feed the classifier
 convert_size = @(x) reshape(x, size(x, 1) * size(x, 2), size(x, 3));
@@ -27,11 +30,40 @@ train_X = convert_size(train_X);
 train_Y = convert_size(train_Y);
 test_X = convert_size(test_X);
 test_Y = convert_size(test_Y);
+X = convert_size(X);
+Y = convert_size(Y);
 
-%% Build model
-NumNeighbors = 5;
-model = ClassificationKNN.fit(train_X, train_Y, 'NumNeighbors', NumNeighbors);
 
-loss(model, test_X, test_Y, 'lossfun', 'classiferror')
-fprintf('Examine the Quality of the KNN Classifier\n')
-fprintf('The classifier predicts incorrectly for %.2f%% of the training data.\n', 100*resubLoss(model));
+%% Examine quiality of MATLAB's KNN classifier (ClassificationKNN)
+fprintf('Examining quality of the MATLAB''s KNN classifier. Define: \n');
+fprintf(['  Resubstitution loss: by default, this is the fraction ', ...
+    'of misclassifications from the predictions of model.\n']);
+fprintf(['  Cross-validation loss: this is the average loss of each cross-', ...
+    'validation model when predicting on data that is not used for training.\n\n']);
+
+% Test using euclidean distance and different number of neighbours
+for NumNeighbors = 2:5
+    fprintf('Model: euclidean distance, %d neighbours\n', NumNeighbors);
+    
+    % Build model
+    model = ClassificationKNN.fit(X, Y, ... % Using all data for this model
+        'NumNeighbors', NumNeighbors, ...
+        'Distance','cosine'); 
+    % Create a cross-validated classifier from the model. 
+    cv_model = crossval(model);
+    
+    % Stats 
+    fprintf('\tResubstitution loss: %.2f%%.\n', 100 * resubLoss(model));
+    fprintf('\tCross-validation loss: %.2f%%.\n', 100 * kfoldLoss(cv_model));
+end
+
+%% Test using euclidean distance and 5 neighbours
+NumNeighbors = 1;
+fprintf('Model: euclidean distance, %d neighbours\n', NumNeighbors);
+% Build model
+model = ClassificationKNN.fit(train_X, train_Y, ... 
+    'NumNeighbors', NumNeighbors, ...
+    'Distance','cosine'); 
+% Test 
+fprintf('\t%d/%d were misclassified\n', numel(find(test_Y ~= predict(model, test_X))), numel(test_Y));
+
